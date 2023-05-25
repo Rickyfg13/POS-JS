44e0a9cb3e43805625442b184b1dee763591c954
Ricky Farela Givano <50465880+Rickyfg13@users.noreply.github.com>
2023-05-25 16:08:02 +0700

    first commit

---
+++ pos-server-api/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const PORT = 3300;
+const app = express()
+
+//swagger
+const options = {
+	definition: {
+		swagger: "2.0",
+		title: "DEACOURSE API DOCS",
+		openapi: "3.1.0",
+		info: {
+			title: "DEACOURSE API DOCS",
+			description: "POS-SERVER API Documentation For CuyUniverse Perople"
+		},
+		schemes: ["dev-sandbox"]
+	},
+	apis: ["./routers/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+	"/docs",
+	swaggerUi.serve,
+	swaggerUi.setup(specs, { explorer: true })
+);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//test
+app.get("/", (req, res) => {
+	res.send("API Ready To GO!");
+});
+
+const product = require("./routers/products");
+const transactions = require("./routers/transactions");
+
+app.use("/products", product);
+app.use("/transactions", transactions);
+
+app.listen(PORT, () => {
+	console.log(`Server running on http://localhost:${PORT}`);
+});
